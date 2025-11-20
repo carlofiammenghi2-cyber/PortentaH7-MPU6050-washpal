90292fd3f1bfd5aa59a819720d15a2c7738bb837
AleGue4 <aleguerrisi01@gmail.com>
2025-11-20 18:47:56 +0100

    Update AM312_Pir_Sensor_example.ino
    Added motion detection with cooldown and no motion detection and emptyness of the room

--- AM312_Pir_Sensor_example/AM312_Pir_Sensor_example.ino
+++ AM312_Pir_Sensor_example/AM312_Pir_Sensor_example.ino
@@ -1,28 +1,52 @@
-// int ledPin = 13;        // onboard LED
-int pirPin = 2;         // PIR sensor output connected to D2
+int pirPin = 2;
 
-int pirState = LOW;     // current state of PIR output
-int val = 0;            // variable for reading PIR status
+int pirState = LOW;
+int val = 0;
+
+unsigned long motionDetectedTime = 0; // Time when motion was first detected
+unsigned long noMotionStartTime = 0;  // Time when motion ended
+unsigned long emptyDelay = 10000;    // 10 seconds delay for empty room message (just for testing, maybe it could be useful to use 30s)
+unsigned long presenceDelay = 5000;   // 5 seconds for presence confirmation
+
+bool presenceReported = false;
+bool motionActive = false;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);     
-  pinMode(pirPin, INPUT);      
-  Serial.begin(9600);          
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(pirPin, INPUT);
+  Serial.begin(9600);
 }
 
-void loop(){
-  val = digitalRead(pirPin);   // read PIR sensor output
-  if (val == HIGH) {           
-    if (pirState == LOW) {
+void loop() {
+  val = digitalRead(pirPin);
+  unsigned long currentTime = millis();
+
+  if (val == HIGH) {
+    if (!motionActive) {
+      motionDetectedTime = currentTime; // first detection
+      presenceReported = false; // reset presence report for new motion
+      motionActive = true;
       Serial.println("Motion detected!");
-      digitalWrite(LED_BUILTIN, LOW); // turn LED ON
-      pirState = HIGH;
+      digitalWrite(LED_BUILTIN, LOW);
+    } else {
+      // If motion continues, check if presence should be confirmed
+      if (!presenceReported && (currentTime - motionDetectedTime >= presenceDelay)) {
+        Serial.println("Someone is in the room");
+        presenceReported = true;
+      }
     }
   } else {
-    if (pirState == HIGH){
+    if (motionActive) {
       Serial.println("Motion stopped!");
-      digitalWrite(LED_BUILTIN, HIGH);  // turn LED OFF
-      pirState = LOW;
+      digitalWrite(LED_BUILTIN, HIGH);
+      motionActive = false;
+      noMotionStartTime = currentTime;
+    }
+
+    // Check if no motion for more than 30 seconds (room empty)
+    if ((currentTime - noMotionStartTime) > emptyDelay) {
+      Serial.println("The washing machine room is empty");
+      noMotionStartTime = currentTime; // prevent repeated messages
     }
   }
 }
